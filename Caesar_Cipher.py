30075a28a71b09d73179eef4ff04b8faf33ebc3f
Samthesurf <ukpsamuel67@gmail.com>
2022-10-23 22:42:12 +0100

    format

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -8,30 +8,28 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 
 
 def caesar(cipher_direction, plaintext, shift_amount):
-    text = ""
-    for char in plaintext:
-      letter_in_list = True
-      if char not in alphabet:
-        text += char
-        letter_in_list = False
-      else:
-        letter_in_list  # The goalhere was too use my mistake from when I was prime checking, I saw Angela's answer and was frustrated
-        # but this exercise of accounting for spaces and symbols helped me realize and recall my mistakes to solve it.
-        position = alphabet.index(char)
-        # in case the number is too large
-        actual_shift = shift_amount % len(alphabet)
-        if direction == 'encode' and letter_in_list:
-          new_position = position + actual_shift
-          # if the position is more than the elements in the alphabet list
-          if new_position > (len(alphabet)-1):
-            new_position -= len(alphabet)
-        elif direction == 'decode' and letter_in_list:
-          new_position = position - actual_shift
-          if new_position < 0:
-            new_position += len(alphabet)
-        new_letter = alphabet[new_position]
-        text += new_letter
-    print(f"The {direction}d text is {text}")
+  text = ""
+  for char in plaintext:
+    letter_in_list = True
+    if char not in alphabet:
+      text += char
+      letter_in_list = False
+    else:
+      letter_in_list  # The goalhere was too use my mistake from when I was prime checking, I saw Angela's answer and was frustrated
+      # but this exercise of accounting for spaces and symbols helped me realize and recall my mistakes to solve it.
+      position = alphabet.index(char)
+      actual_shift = shift_amount % len(alphabet) # in case the number is too large
+      if direction == 'encode' and letter_in_list:
+        new_position = position + actual_shift
+        if new_position > (len(alphabet)-1): # if the position is more than the elements in the alphabet list
+          new_position -= len(alphabet)
+      elif direction == 'decode' and letter_in_list:
+        new_position = position - actual_shift
+        if new_position < 0:
+          new_position += len(alphabet)
+      new_letter = alphabet[new_position]
+      text += new_letter
+  print(f"The {direction}d text is {text}")
 
 
 choice = True
